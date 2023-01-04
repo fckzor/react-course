b965e0053a2364440de3e3ede432595ad122cae4
fckzor <iptokmach@gmail.com>
2023-01-04 23:39:18 +0200

    module03/lesson/07/work-with-array

--- src/App.jsx
+++ src/App.jsx
@@ -2,50 +2,20 @@ import React from 'react'
 
 class App extends React.Component {
   state = {
-    count: 0,
-    isCounting: false,
+    posts: [
+      {id: 1, name: 'JavaScript'},
+      {id: 2, name: 'Vue'},
+      {id: 3, name: 'React'},
+    ],
   }
-  toggleTimer() {
-    this.setState({isCounting: !this.state.isCounting})
-
-    !this.state.isCounting 
-      ? this.startTimer()
-      : this.stopTimer()
-  }
-  startTimer() {
-    this.timerId = setInterval(() => {
-      this.setState({count: this.state.count + 1})
-    }, 1000)
-  }
-  stopTimer() {
-    clearInterval(this.timerId)
-  }
-  onResetCounter() {
-    this.stopTimer()
-    this.setState({isCounting: false, count: 0})
-    localStorage.setItem('timer', JSON.stringify(0))
-  }
-  componentDidMount() {
-    const timer = JSON.parse(localStorage.getItem('timer'))
-    this.setState({count: timer ? timer : 0})
-  }
-  componentDidUpdate() {
-    localStorage.setItem('timer', JSON.stringify(this.state.count))
-  }
-  componentWillUnmount() {
-    this.stopTimer()
-  }
+  
   render() {
     return (
       <div className="container">
-        <h1>Timer</h1>
-        <div>
-          <div className="counter-view">{this.state.count}</div>
-          <div>
-            <button onClick={() => this.toggleTimer()}>{this.state.isCounting ? 'Stop' : 'Start'}</button>
-            <button onClick={() => this.onResetCounter()}>Reset</button>
-          </div>
-        </div>
+        <h1>Books</h1>
+        {this.state.posts.map(post => (
+          <h4 key={post.id}>{post.name}</h4>
+        ))}
       </div>
     )
   }
