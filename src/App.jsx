ad6a88d92ab89b21fe1e4fa0ba57d556581d1add
fckzor <iptokmach@gmail.com>
2023-01-04 23:16:03 +0200

    module03/lesson06/refactor

--- src/App.jsx
+++ src/App.jsx
@@ -3,50 +3,38 @@ import React from 'react'
 class App extends React.Component {
   state = {
     count: 0,
-    status: 'stopped',
-    startBtnText: 'Start',
+    isCounting: false,
   }
-
   toggleTimer() {
-    this.setState({status: this.state.status === 'stopped' ? 'started' : 'stopped'})
-    this.setState(
-      (prevState) => ({startBtnText: prevState.status === 'started' ? 'Stop' : 'Start'}),
-      () => {
-        this.state.status === 'started' ? this.startTimer() : this.stopTimer()
-      }
-    )
-  }
+    this.setState({isCounting: !this.state.isCounting})
 
+    !this.state.isCounting 
+      ? this.startTimer()
+      : this.stopTimer()
+  }
   startTimer() {
-    this.timer = setInterval(() => {
+    this.timerId = setInterval(() => {
       this.setState({count: this.state.count + 1})
     }, 1000)
   }
-
   stopTimer() {
-    clearInterval(this.timer)
+    clearInterval(this.timerId)
   }
-
   onResetCounter() {
-    this.setState(
-      {count: 0}, 
-      () => { localStorage.setItem('timer', JSON.stringify(this.state.count)) }
-    )
+    this.stopTimer()
+    this.setState({isCounting: false, count: 0})
+    localStorage.setItem('timer', JSON.stringify(0))
   }
-
   componentDidMount() {
     const timer = JSON.parse(localStorage.getItem('timer'))
     this.setState({count: timer ? timer : 0})
   }
-
   componentDidUpdate() {
     localStorage.setItem('timer', JSON.stringify(this.state.count))
   }
-
   componentWillUnmount() {
     this.stopTimer()
   }
-
   render() {
     return (
       <div className="container">
@@ -54,7 +42,7 @@ class App extends React.Component {
         <div>
           <div className="counter-view">{this.state.count}</div>
           <div>
-            <button onClick={() => this.toggleTimer()}>{this.state.startBtnText}</button>
+            <button onClick={() => this.toggleTimer()}>{this.state.isCounting ? 'Stop' : 'Start'}</button>
             <button onClick={() => this.onResetCounter()}>Reset</button>
           </div>
         </div>
