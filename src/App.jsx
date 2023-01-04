0e03379f4d651904316deaabbe1a0de6bea1bb00
fckzor <iptokmach@gmail.com>
2023-01-04 22:46:05 +0200

    module03/lesson06/create-timer

--- src/App.jsx
+++ src/App.jsx
@@ -3,41 +3,59 @@ import React from 'react'
 class App extends React.Component {
   state = {
     count: 0,
-    counterStep: 1,
+    status: 'stopped',
+    startBtnText: 'Start',
   }
 
-  changeCounter(action) {
-    this.setState({
-      count: action === 'increment' 
-        ? this.state.count + this.state.counterStep
-        : action === 'decrement' 
-          ? this.state.count - this.state.counterStep
-          : this.state.count
-    })
+  toggleTimer() {
+    this.setState({status: this.state.status === 'stopped' ? 'started' : 'stopped'})
+    this.setState(
+      (prevState) => ({startBtnText: prevState.status === 'started' ? 'Stop' : 'Start'}),
+      () => {
+        this.state.status === 'started' ? this.startTimer() : this.stopTimer()
+      }
+    )
   }
 
-  changeCounterStep(event) {
-    const target = Number(event.target.value)
-    this.setState({counterStep: target > 1 ? target : 1})
+  startTimer() {
+    this.timer = setInterval(() => {
+      this.setState({count: this.state.count + 1})
+    }, 1000)
+  }
+
+  stopTimer() {
+    clearInterval(this.timer)
+  }
+
+  onResetCounter() {
+    this.setState(
+      {count: 0}, 
+      () => { localStorage.setItem('timer', JSON.stringify(this.state.count)) }
+    )
+  }
+
+  componentDidMount() {
+    const timer = JSON.parse(localStorage.getItem('timer'))
+    this.setState({count: timer ? timer : 0})
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('timer', JSON.stringify(this.state.count))
+  }
+
+  componentWillUnmount() {
+    this.stopTimer()
   }
 
   render() {
     return (
       <div className="container">
-        <h1>Hello! Change counter!</h1>
-        <div className="counter-wrapper">
-          <label>
-            <span>Set counter step</span>
-            <input 
-              type="number"
-              value={this.state.counterStep}
-              onChange={(e) => this.changeCounterStep(e)}
-            />
-          </label>
+        <h1>Timer</h1>
+        <div>
+          <div className="counter-view">{this.state.count}</div>
           <div>
-            <button onClick={() => this.changeCounter('decrement')}>-</button>
-            <span className="counter-view">{this.state.count}</span>
-            <button onClick={() => this.changeCounter('increment')}>+</button>
+            <button onClick={() => this.toggleTimer()}>{this.state.startBtnText}</button>
+            <button onClick={() => this.onResetCounter()}>Reset</button>
           </div>
         </div>
       </div>
