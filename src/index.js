cfb3d8ee104fa0b58efd6b7208fd4fbb8add1763
fckzor <iptokmach@gmail.com>
2023-01-05 14:13:12 +0200

    add clear project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'reset-css'
 import './style.css'
 import App from './App'
-
-let isLoading = false
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App isLoading={isLoading} />
+    <App />
   </React.StrictMode>
 )
