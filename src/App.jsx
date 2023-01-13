0b6091a22d7b967dca23c99aa8b017c980b08418
fckzor <iptokmach@gmail.com>
2023-01-13 07:01:34 +0200

    module06/lesson01/add-clicker

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const increement = () => setCount(count + 1)
+  const decreement = () => setCount(count - 1)
+  
   return (
-    <div>
-      
+    <div className="container">
+      <div className="count-wrap">
+        <button onClick={decreement}>-</button>
+        <span>{count}</span>
+        <button onClick={increement}>+</button>
+      </div>
     </div>
   )
 }
